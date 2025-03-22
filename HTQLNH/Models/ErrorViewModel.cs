92e1d96ecea2835f56a3583e1607ac008ecdec60
khangfish <khangkhokhao687@gmail.com>
2025-03-22 11:02:09 +0700

    He thong quan ly nha hang

---
+++ HTQLNH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTQLNH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
